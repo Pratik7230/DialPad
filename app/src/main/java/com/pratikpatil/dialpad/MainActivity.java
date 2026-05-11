8bf76c4d7771dc72a9990adb53f99b679dafb5ee
Pratik Patil <Pratik7230@GMAIL.COM>
2026-05-11 22:50:02 +0530

    bottom bar added

--- app/src/main/java/com/pratikpatil/dialpad/MainActivity.java
+++ app/src/main/java/com/pratikpatil/dialpad/MainActivity.java
@@ -8,9 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +28,25 @@ public class MainActivity extends AppCompatActivity {
         phoneNumber = findViewById(R.id.phoneNumber);
         btnCall = findViewById(R.id.btnCall);
         btnDelete = findViewById(R.id.btnDelete);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setSelectedItemId(R.id.nav_call);
+        bottomNav.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_history) {
+                Toast.makeText(this, R.string.toast_history_coming_soon, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            if (itemId == R.id.nav_contacts) {
+                Toast.makeText(this, R.string.toast_contacts_coming_soon, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            if (itemId == R.id.nav_settings) {
+                Toast.makeText(this, R.string.toast_settings_coming_soon, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return true;
+        });
 
         btnDelete.setOnClickListener(v -> {
             String text = phoneNumber.getText().toString();
