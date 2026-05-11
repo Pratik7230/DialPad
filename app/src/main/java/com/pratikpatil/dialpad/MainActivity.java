a27ee466291c1577edd885838596c65bb049933d
Pratik Patil <Pratik7230@GMAIL.COM>
2026-05-11 22:19:14 +0530

    basic ui logic

--- app/src/main/java/com/pratikpatil/dialpad/MainActivity.java
+++ app/src/main/java/com/pratikpatil/dialpad/MainActivity.java
@@ -1,24 +1,65 @@
 package com.pratikpatil.dialpad;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText phoneNumber;
+    Button btnCall, btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        phoneNumber = findViewById(R.id.phoneNumber);
+        btnCall = findViewById(R.id.btnCall);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        btnDelete.setOnClickListener(v -> {
+            String text = phoneNumber.getText().toString();
+
+            if (!text.isEmpty()) {
+                phoneNumber.setText(text.substring(0, text.length() - 1));
+            }
         });
+
+        btnCall.setOnClickListener(v -> {
+            String number = phoneNumber.getText().toString();
+
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:" + number));
+
+            if (ActivityCompat.checkSelfPermission(
+                    MainActivity.this,
+                    Manifest.permission.CALL_PHONE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        1);
+
+                return;
+            }
+
+            startActivity(intent);
+        });
+    }
+
+    public void numberClick(View view) {
+        Button button = (Button) view;
+        String current = phoneNumber.getText().toString();
+        phoneNumber.setText(current + button.getText().toString());
     }
 }
